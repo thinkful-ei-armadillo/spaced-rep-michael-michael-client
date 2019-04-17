5ba34a57f14a14b970f5c0445c124de29ac2af86
Michael Bramble <mike.w.bramble@gmail.com>
2019-04-17 15:05:15 -0400

    learningroute

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -6,48 +6,22 @@ class LearningRoute extends Component {
   
   state = {
     total_score: null,
-    nextWord: [
-      {
-        original: null,
-        correct_count: null,
-        incorrect_count: null,
-      }
-  
-    ]
-    
+    words: [{
+      id: null,
+      original: null,
+      translation: null,
+      correct_count: 0,
+      incorrect_count: 0
+    }],
+    currentWord: 1,
+    guess: ''
   }
 
   componentDidMount (){
-    // this.getTotalScore();
-    this.getNextWord();
-    // this.fakeWord();
+    this.getWords();
   }
 
-  // getTotalScore(){
-  //   fetch(`${config.API_ENDPOINT}/language/head`, {
-  //     method: "GET",
-  //     headers: {
-  //       "content-type": "application/json",
-  //       "Authorization": `Bearer ${TokenService.getAuthToken()}`
-  //     }
-  //   })
-  //   .then(res => {
-  //     if(!res.ok){
-  //       return 'error';
-  //     }
-  //     return res.json();
-  //   })
-  //   .then(res => {
-  //     this.setState({
-        
-  //         total_score: res.language.total_score
-        
-  //     })
-      
-  //   })
-  // }
-
-  getNextWord(){
+  getWords(){
     fetch(`${config.API_ENDPOINT}/language/head`, {
       method: "GET",
       headers: {
@@ -63,55 +37,94 @@ class LearningRoute extends Component {
     })
     .then(res => {
       this.setState({
-        
           total_score: res.language.total_score,
-          nextWord: res.nextWord,
-          
-        
+          words: res.nextWord,
       })
-      //  console.log(this.state.nextWord);
-      //  console.log(this.state.nextWord[0].original);
     })
-
   }
 
-  
-  
+  showWord(){
+    for(let i = (this.state.currentWord - 1); i < this.state.currentWord; i++){
+      return(
+        <div className="currentWord">
+          <span>{this.state.words[i].original}</span>
+          <p>
+            You have answered this word correctly {this.state.words[i].correct_count} times.
+          </p>
+          <p>
+            You have answered this word incorrectly {this.state.words[i].incorrect_count} times.
+          </p>
+        </div>
+      )
+    }
+  }
 
+  handleChange = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+    this.handleState(e.target.value);
+  }
 
+  handleState(guess){
+    this.setState({guess: guess});
+  }
+
+  handleGuess = e => {
+    e.preventDefault();
+    if(this.state.words[this.state.currentWord - 1].translation === this.state.guess){
+      console.log('CORRECT')
+      fetch(`${config.API_ENDPOINT}/language/guess`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "Authorization": `Bearer ${TokenService.getAuthToken()}`
+        },
+        body: {
+          word: this.state.words[this.state.currentWord - 1].id,
+          guess: 'correct'
+        }
+      })
+      .then(res => {
+        if(!res.ok){
+          return 'error';
+        }
+        return res.json();
+      })
+      .then(res => {
+        console.log(res)
+      })
+    }
+    else{
+      console.log('INCORRECT')
+    }
+    
+    // cycle through the array of words:
+    // let nextWord = this.state.currentWord + 1;
+    // if(nextWord <= this.state.words.length){
+    //   this.setState({currentWord: nextWord});
+    // }
+    // else{
+    //   this.setState({currentWord: 1})
+    // }
+  }
 
   render() {
-    
-    
-   
-
-     const newWord  = this.state.nextWord;
-
-    // console.log(newWord);
-    
     return (
       <section>
         <h2>Translate The Word:</h2>
-        <span>Andar</span>
-        <p>Your total score is {this.state.total_score}</p>
-        <form>
-        <fieldset>
-         <label htmlFor='nextword'>What's the translation for this word?</label>
-         <input type='text' name='nextword' required></input>
-        </fieldset>
-        <button type='reset'>Reset</button>
-        <button type='submit'>Submit your answer</button>
+        {this.showWord()}
+        <p>Total Score: {this.state.total_score}</p>
+        <form onSubmit={(e) => {this.handleGuess(e)}}>
+          <fieldset>
+            <label htmlFor='nextword'>What's the translation for this word?</label>
+            <input type='text' name='nextword' id="userGuess" onChange={this.handleChange} required></input>
+          </fieldset>
+          <button type='reset'>Reset</button>
+          <button type='submit'>Submit Your Guess</button>
         </form>
-        <div className='word-results'>
-        <p>You have answered this word correctly {newWord[0].correct_count} times</p>
-        <p>You have answered this word incorrectly {newWord[0].incorrect_count} times</p>
-        
-        </div>
-
-
       </section>
     );
   }
 }
 
-export default LearningRoute
+export default LearningRoute;
