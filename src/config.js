baa811b560419252a4f80a0c1961f4bcdeff1a59
mdb1710 <mdb1710@gmail.com>
2019-04-18 17:09:52 -0400

    revision of env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_API_BASE_URL || 'http://localhost:8000/api',
   TOKEN_KEY: process.env.TOKEN_KEY,
 }
 
